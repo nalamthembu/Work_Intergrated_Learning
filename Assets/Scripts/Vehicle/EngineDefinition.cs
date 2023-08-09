801d6d1fd7fa8ca2b478cd373e3619917e3e45b7
Nala Mthembu <Nala00.12@gmail.com>
2023-08-09 19:36:27 +0200

    Refactoring

--- Assets/Scripts/Vehicle/EngineDefinition.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "EngineDefinition", menuName = "Game/Vehicle/Engine Definition")]
-public class EngineDefinition : ScriptableObject
-{
-    public AnimationCurve torqueCurve;
-}
