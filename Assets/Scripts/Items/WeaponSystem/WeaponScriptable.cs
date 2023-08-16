b06564b2011b30a6108daf52d7566e6c83fb9ec4
Nala Mthembu <Nala00.12@gmail.com>
2023-08-16 21:07:00 +0200

    Player Animation & Weapon/Item System
    Putting Weapon/Item System in place.
    Weapons are items and inherit from the item class.

---
+++ Assets/Scripts/Items/WeaponSystem/WeaponScriptable.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Weapon", menuName = "Game/Weapon/WeaponData")]
+public class WeaponScriptable : ScriptableObject
+{
+    new public string name;
+    public float range = 50; //in metres
+    public float maxClip = 30; //max ammo in one single clip
+    public float damage = 10;
+    public float fireRate = 5;
+
+    public FIRE_TYPE fireType;
+
+    [Header("Resting")]
+    public Vector3 restingPosition;
+    public Vector3 restingRotation;
+
+    [Header("Aiming")]
+    public Vector3 aimingPosition;
+    public Vector3 aimingRotation;
+}
+
+public enum FIRE_TYPE
+{
+    SEMIAUTO,
+    AUTO
+}
