6ee16d7d0546893196c286d18150fcb34abb7125
CellHanKS <88459746+CellHanKS@users.noreply.github.com>
2023-10-26 11:13:24 +0200

    huh huh

---
+++ Assets/Scripts/LevelDesign/Interactables/ComputerInteractable.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerInteractable : MonoBehaviour
+{
+    const string PLAYER_TAG = "Player";
+
+    [SerializeField] GameObject CPU_Screen;
+}
